67f8962e0df4268a271eca974f6d9b5cd77666b6
sithell <thesithell@gmail.com>
2021-12-01 22:33:39 +0300

    feature: add letter template

---
+++ lib/utils.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+/**
+ * @throws Exception
+ */
+function generateLetter(
+    string $city,
+    string $phone,
+    string $address,
+    string $area,
+    string $type,
+    array $extras,
+    string $comment,
+    int $price,
+    string $raw,
+    string $date,
+): string
+{
+    if (!$template = file_get_contents(__DIR__ . '/../templates/order.min.html')) {
+        throw new Exception("File templates/order.min.html not found");
+    }
+
+    $template = preg_replace('{{city}}', $city, $template);
+    $template = preg_replace('{{phone}}', $phone, $template);
+    $template = preg_replace('{{address}}', $address, $template);
+    $template = preg_replace('{{area}}', $area, $template);
+    $template = preg_replace('{{type}}', $type, $template);
+    $template = preg_replace('{{comment}}', $comment, $template);
+    $template = preg_replace('{{price}}', strval($price), $template);
+    $template = preg_replace('{{raw}}', $raw, $template);
+    $template = preg_replace('{{date}}', $date, $template);
+
+    $extraString = '';
+    foreach ($extras as $extra) {
+        $extraString .= "<li>$extra</li>";
+    }
+    $template = preg_replace('{{extra}}', $extraString, $template);
+
+    return $template;
+}
--- public/index.php
+++ public/index.php
@@ -9,6 +9,7 @@ use Slim\Http\StatusCode;
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../lib/google.php';
+require __DIR__ . '/../lib/utils.php';
 
 $config = include('../config/app.php');
 
@@ -16,12 +17,55 @@ $app = new App;
 
 $app->post('/order', function (Request $request, Response $response, array $args) use ($config) {
     $body = $request->getParsedBody();
+    $city = $body['city'] ?? '-';
+    $phone = $body['phone'] ?? '-';
+    $typeOfCleaning = $body['type_of_cleaning'] ?? '-';
+    $plan = $body['plan'] ?? '-';
+    $totalPrice = (int)($body['total_price'] ?? 0);
+    $comment = $body['comment'] ?? '-';
+
+    $street = $body['street'] ?? '';
+    $house = $body['house'] ?? '';
+    $flat = $body['flat'] ?? null;
+    $address = '-';
+    if (!empty($street)) {
+        $address = $street;
+        if (!empty($house)) {
+            $address .= ' д. ' . $house;
+            if (!empty($flat)) {
+                $address .= ' кв. ' . strval($flat);
+            }
+        }
+    }
+
+    $extras = (array)($body['extras'] ?: []);
+    $extrasParsed = [];
+    foreach ($extras as $extra) {
+        $extrasParsed[] = $extra['name'] . ' x' . $extra['quantity'];
+    }
+
+    $date = date('d.m.Y H:i:s');
+    $raw = json_encode($body);
+
+    $messageText = generateLetter(
+        $city,
+        $phone,
+        $address,
+        $plan,
+        $typeOfCleaning,
+        $extrasParsed,
+        $comment,
+        $totalPrice,
+        $raw,
+        $date
+    );
+
     try {
         sendMessage(
             $config['mail']['from'],
             $config['mail']['to'],
             $config['mail']['subject'],
-            json_encode($body, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT),
+            $messageText,
         );
         $response = $response->withJson(['status' => 'ok'], StatusCode::HTTP_OK);
     } catch (Exception $e) {
